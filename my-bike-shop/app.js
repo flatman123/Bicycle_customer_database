0c1c552e9ee413b4436875ff4210dd10db4095c8
Jeffrey McIntyre <flatlandpyhon67@hotmail.com>
2019-05-26 09:41:26 -0700

    Added EventListeners for submit buttons, added dataContructor for employeeDatabase, made domStrings public

--- my-bike-shop/app.js
+++ my-bike-shop/app.js
@@ -1,34 +1,85 @@
 var Bicycle_main = (function(username, password) {
-	var userID;
+	var employeeID, empDatabase, customerInfo;
+
 	this.username = username;
 	this.password = password;
-	
-	return {
-		publicAccess: function(u,p) {
-			console.log(u + p);
-			
-		}
-	}
-})();
+	this.employeeID = Math.random(0) * 99999;
 
+
+	empDatabase = { 
+			jmcintyre: {ID: employeeID, password: 'cisco123'}
+	};
+
+	return 5;
+})();
 
 
 var uiController = (function() {
 	var domStrings;
 
 	domStrings = {
-		user: '.l_user';
-		pass: '.l_pass'
+		user: '.l_user',
+		pass: '.l_pass',
+		loginBtn: '.signinBtn'
 	};
-	
 
+
+
+	return {
+		getUserCredentials: function() {
+			//Get user Credentials from login page
+			return {
+				username: document.querySelector(domStrings.user).value,
+				password: document.querySelector(domStrings.pass).value
+			};
+		},
+		sendDomStrings: function() {
+			return domStrings;
+		}
+	}
 })();
 
 
-
 var appController = (function(uiCtrl, bikeMain) {
+	var getDoms, getEventListeners,usrCredentials,doms;
 
-	bikeMain.publicAccess('jmcintyre', 'Jsndf89h24@');
+	doms = uiCtrl.sendDomStrings();
+	
+	function getEventListeners(){
 
+		document.querySelector(doms.loginBtn).addEventListener('click',sendCredentials);
+		document.addEventListener('keypress', function(kpEvent) {
+
+		if (kpEvent.keyCode === 13 || kpEvent.which === 13) {
+
+			if (doms.user !== '' && doms.pass !== '') {
+				sendCredentials();
+			} else if (doms.user !== '' && doms.pass === '') {
+				alert('Please Enter a Password Before Submitting!');
+			} else if (doms.user === '' && doms.pass !== '') {
+				alert('Please Enter your Username!');
+			} else {
+				alert('Please enteryour credentials!');
+			}
+		}
+		console.log(kpEvent.keyCode);
+	});
+	};
+
+
+	function sendCredentials() {
+
+	//1. Get the Employee login information
+	usrCredentials = uiCtrl.getUserCredentials();
+	console.log(usrCredentials);
+	};
+
+	return {
+		run: function() {
+			getEventListeners();
+		}
+	};
 
 })(uiController, Bicycle_main);
+
+appController.run();
